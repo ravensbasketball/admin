87e75b837b18d76d2b929dff61aebc01acfa784c
Richard Herbert <richard@infoweb.co.uk>
2026-01-01 15:27:22 +0000

    Added players

---
+++ src/routes/players/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { page } from '$app/state';
+	export let data;
+</script>
+
+<h1>Players</h1>
+
+<ul>
+	<li>here</li>
+	{#each data.players as player}
+		<li>{player.givenName}</li>
+	{/each}
+</ul>
+
+
+<h1>Route - { page.url.pathname }</h1>
