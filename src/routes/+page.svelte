55c32e2791eab564e33def55ddaa66d8b8ac292c
Richard Herbert <richard@infoweb.co.uk>
2026-01-01 15:27:45 +0000

    Added Supabase connections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { page } from '$app/state';
+	export let data;
+</script>
+
+<h1>Players</h1>
+
+<ul>
+	{#each data.players as player}
+		<li>{player.givenName}</li>
+	{/each}
+</ul>
+
+
+<h1>Route - { page.url.pathname }</h1>
