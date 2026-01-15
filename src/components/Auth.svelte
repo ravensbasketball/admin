6072a0f42bca6cd28235feed1eb01d2edf2d0f11
Richard Herbert <richard@infoweb.co.uk>
2026-01-15 10:34:54 +0000

    Added signin and signout

---
+++ src/components/Auth.svelte
@@ -0,0 +1,13 @@
+<h1>Sign In</h1>
+
+<form action="/signin" method="POST">
+	<label for="email">Email address</label>
+	<input id="email" name="email" type="email" required>
+
+	<label for="password">Password</label>
+	<input id="password" name="password" type="password" required>
+
+	<br>
+
+	<button type="submit">Sign In</button>
+</form>
