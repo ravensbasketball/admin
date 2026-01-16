0befa60bd03e68873a6e8ce24f9b954f2038ec3e
Richard Herbert <richard@infoweb.co.uk>
2026-01-16 17:16:04 +0000

    Applied SupaBase SvelteKit example
    https://github.com/supabase/supabase/tree/master/examples/user-management/sveltekit-user-management

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,17 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
+import { SupabaseClient, Session } from '@supabase/supabase-js'
+// See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+		interface Locals {
+			supabase: SupabaseClient
+			safeGetSession(): Promise<{ session: Session | null; user?: Session["user"] | null }>
+		}
+		interface PageData {
+			session: Session | null
+			user?: Session["user"] | null
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
